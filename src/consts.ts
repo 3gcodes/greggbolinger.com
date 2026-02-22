6729ac2beb93e2703340e1cf06760b3f33d8de69
Gregg Bolinger <gregg.bolinger@skaivision.com>
2026-02-22 16:04:55 -0600

    added article

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = 'Gregg Bolinger';
-export const SITE_DESCRIPTION = 'Personal blog and website of Gregg Bolinger.';
+export const SITE_TITLE = 'Sakriphyce';
+export const SITE_DESCRIPTION = 'Personal blog and website of Sakriphyce.';
